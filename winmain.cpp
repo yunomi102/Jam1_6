c36f38b770b5de9bd8e9f74e012042bd31e39cf7
Chino Masato <56014502+ChinoMasato@users.noreply.github.com>
2023-06-27 09:14:17 +0900

    プロジェクト ファイルを追加します。

---
+++ winmain.cpp
@@ -0,0 +1,31 @@
+﻿#include "DxLib.h"
+
+// プログラムは WinMain から始まります
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	//DXライブラリの初期化処理 ここから
+	ChangeWindowMode(TRUE);	//ウィンドウモードにする
+	SetGraphMode(800, 600, 32);	//ウィンドウサイズを設定する
+	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+	SetDrawScreen(DX_SCREEN_BACK);	//裏画面を描画対象へ
+	//DXライブラリの初期化処理 ここまで
+
+
+	//メイン処理
+	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
+	{
+		//ドット描画処理
+		int color = GetColor(255, 255, 255);
+		DrawPixel(400, 300, color);
+		ScreenFlip();		//裏画面と表画面の入替
+		ClearDrawScreen();	//裏画面の描画を全て消去
+	}
+
+	DxLib_End();			// ＤＸライブラリ使用の終了処理
+
+	return 0;				// ソフトの終了 
+}
+
